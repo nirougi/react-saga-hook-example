b4af7097d96a90c6dc02fc0a407cd7afbbd63b92
nirougi <jinik.nr@gmail.com>
2020-11-26 20:13:49 +0000

    Deploying to gh-pages from  @ a13b92b1166fb5708d24806ee4c405384b785224 🚀

---
+++ precache-manifest.07cf29627ae80a66e9305c388b39412b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9535b0528bc1f67f27cbf0472cd5e8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1304231816d3f117b2b2",
+    "url": "/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "d4c4b762ceaeb8a3cb09",
+    "url": "/static/css/main.7c9c8426.chunk.css"
+  },
+  {
+    "revision": "1304231816d3f117b2b2",
+    "url": "/static/js/2.f1a176ec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f1a176ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c4b762ceaeb8a3cb09",
+    "url": "/static/js/main.6b16f5e8.chunk.js"
+  },
+  {
+    "revision": "8b7408a12cf656b9c264",
+    "url": "/static/js/runtime-main.798e7351.js"
+  }
+]);
